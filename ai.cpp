e002371d91182013b4ca5975e6ceaee0b31413a8
wangqr <wqr.prg@gmail.com>
2015-05-03 16:44:56 +0800

    v1.0

--- ai.cpp
+++ ai.cpp
@@ -39,7 +39,7 @@ inline bool InSight (const Position pos)						{for(int i=0;i<INFO->element_num;+
 inline bool InFireRange(const State& a, const Position pos)	{return Distance(a.pos,pos)<=kProperty[a.type].fire_ranges[pos.z];}
 inline bool InFireRange(const State& a, const State& b)		{if((kProperty[a.type].attacks[0]==0 || kProperty[b.type].defences[0]==-1) &&(kProperty[a.type].attacks[1]==0 || kProperty[b.type].defences[1]==-1)) return false;return InFireRange(a,b.pos);}
 inline bool InMap(int x,int y)								{return 0<=x && x<INFO->x_max && 0<=y && y<INFO->y_max;}
-inline bool InMap(Position pos)								{return InMap(pos.x,pos.y);}
+inline bool InMap(const Position& pos)						{return InMap(pos.x,pos.y);}
 Position operator+ (const Position& a, const Position& b)	{Position temp;temp.x=a.x+b.x;temp.y=a.y+b.y;temp.z=-1;return temp;}
 Position operator- (const Position& a, const Position& b)	{Position temp;temp.x=a.x-b.x;temp.y=a.y-b.y;temp.z=-1;return temp;}
 struct PosComp												{bool operator() (const Position& a, const Position& b)	{return a.x<b.x ||(a.x==b.x && (a.y<b.y || (a.y==b.y && a.z<b.z)));}};
@@ -102,7 +102,10 @@ struct UsedPos : public Position
 struct UsedPosComp
 {
 	bool operator() (const UsedPos& a, const UsedPos& b)	{
-		if(a.round<b.round)return true;
+		if(a.round<b.round)
+			return true;
+		if(a.round>b.round)
+			return false;
 		return a.x<b.x ||(a.x==b.x && (a.y<b.y || (a.y==b.y && a.z<b.z)));
 	}
 };
@@ -113,7 +116,16 @@ enum UnitSignal
 	UNDER_ATTACK
 };
 
-
+inline int GetDamage(const State& source, const State& target){
+	int distance=Distance(source.pos,target.pos);
+	int fire_range = kProperty[source.type].fire_ranges[kProperty[target.type].level];
+	int modified_attacks[2];
+	modified_attacks[0] = int((1 - float(distance - fire_range / 2) / (fire_range + 1)) * kProperty[source.type].attacks[0]);
+	modified_attacks[1] = int((1 - float(distance - fire_range / 2) / (fire_range + 1)) * kProperty[source.type].attacks[1]);
+	int fire_damage = max(0, modified_attacks[0] - kProperty[target.type].defences[0]);
+	int torpedo_damage = max(0, modified_attacks[1] - kProperty[target.type].defences[1]);
+	return fire_damage + torpedo_damage;
+}
 
 struct Job
 {
@@ -125,7 +137,7 @@ struct Job
 enum GlobalSignal
 {
 	CARGO_UNDER_ATTACT,
-	BASE_UNDER_ATTACT,
+	BASE_UNDER_ATTACK,
 	UNIT_LOST,
 	UNIT_GENERATED,
 };
@@ -187,7 +199,7 @@ struct Unit : public State
 			if(team==INFO->team_num){
 				if(health>a.health){
 					sig_add(UNDER_ATTACK);
-					RaiseGsig(BASE_UNDER_ATTACT);
+					RaiseGsig(BASE_UNDER_ATTACK);
 				}
 				else{
 					sig_remove(UNDER_ATTACK);
@@ -237,6 +249,8 @@ struct Base : public Building
 	Position rally_point[kElementTypes]; // [FIXME] no use
 };
 Base* my_base;Base* enemy_base;
+
+
 void Unit::DoJob(){
 	if(!job.empty()){
 		Job a=job.front();
@@ -245,8 +259,11 @@ void Unit::DoJob(){
 				ChangeDest(index,a.pos);
 			}
 			if(InFireRange(*this,*Units[a.target])){
-				AttackUnit(index,a.target);
-				last_command_send_round=INFO->round;
+				if(GetDamage(*Units[index],*Units[a.target])>0){
+					AttackUnit(index,a.target);
+					Units[a.target]->health-=GetDamage(*Units[index],*Units[a.target]);
+					last_command_send_round=INFO->round;
+				}
 			}
 		}
 	}
@@ -258,10 +275,16 @@ void Unit::DoJob(){
 				enemy_in_sight.push_back(j->index);
 			}
 		}
-		if(!enemy_in_sight.empty()){
-			// [TODO] attacking order
-			AttackUnit(index,enemy_in_sight.front());
+		for(unsigned i=0;i<enemy_in_sight.size();++i){
+			if(Units[enemy_in_sight[i]]->health<=0) continue;
+			int t=GetDamage(*this,*Units[enemy_in_sight[i]]);
+			if(t>0){
+				AttackUnit(index,enemy_in_sight[i]);
+				Units[enemy_in_sight[i]]->health-=t;
+				break;
+			}
 		}
+		/*
 		else{
 			enemy_in_sight.clear();
 			for(int i=0;i<INFO->element_num;++i){
@@ -275,6 +298,7 @@ void Unit::DoJob(){
 				AttackUnit(index,enemy_in_sight.front());
 			}
 		}
+		*/
 	}
 }
 struct Fort : public Building
@@ -335,9 +359,7 @@ struct OilField : public Resources
 Position FindMetalPos(Position pos){
 	Position a={0,0,-1};
 	UsedPos q;
-	if(metalPos.empty())return a;
-	a=metalPos.begin()->first;
-	int dis=RouteDis(pos,a);
+	int dis=INT_MAX;
 	for(map<Position,int,PosComp>::iterator i=metalPos.begin();i!=metalPos.end();++i){
 		int t=RouteDis(pos,i->first);
 		q.x=i->first.x;
@@ -359,9 +381,7 @@ Position FindMetalPos(Position pos){
 Position FindFuelPos(Position pos){
 	Position a={0,0,-1};
 	UsedPos q;
-	if(fuelPos.empty())return a;
-	a=fuelPos.begin()->first;
-	int dis=RouteDis(pos,a);
+	int dis=INT_MAX;
 	for(map<Position,int,PosComp>::iterator i=fuelPos.begin();i!=fuelPos.end();++i){
 		int t=RouteDis(pos,i->first);
 		q.x=i->first.x;
@@ -381,9 +401,9 @@ Position FindFuelPos(Position pos){
 }
 
 Position FindWayHome(Position pos){
-	Position a=*oceanNearMyBase.begin();
+	Position a={0,0,-1};
 	UsedPos q;
-	int dis=RouteDis(pos,a);
+	int dis=INT_MAX;
 	for(set<Position,PosComp>::iterator i=oceanNearMyBase.begin();i!=oceanNearMyBase.end();++i){
 		int t=RouteDis(pos,*i);
 		q.x=i->x;
@@ -399,6 +419,7 @@ Position FindWayHome(Position pos){
 			dis=t;
 		}
 	}
+	printf("[DEBUG] finally decided (%d,%d,%d)\n",a.x,a.y,a.z);
 	return a;
 }
 
@@ -466,7 +487,7 @@ struct Cargo : public Ship
 				}
 				else{ // [FIXME] jump some process
 					Position q=FindFuelPos(pos);
-					int fn=RouteDis(pos,q)+10;
+					int fn=RouteDis(pos,q)+20;
 					Supply(index,my_base->index,fuel-fn,0,metal);
 					a.type=a.COLLECT;
 					a.pos=q;
@@ -484,7 +505,7 @@ struct Cargo : public Ship
 			q.x=a.pos.x;
 			q.y=a.pos.y;
 			q.z=a.pos.z;
-			q.round=INFO->round+(RouteDis(pos,a.pos)-1)/kProperty[CARGO].speed;
+			q.round=INFO->round+(RouteDis(pos,a.pos)-1)/kProperty[type].speed;
 			printf("[DEBUG] used pos (%d,%d,%d,%d)\n",q.x,q.y,q.z,q.round);
 			usedPos[q]=index;
 			++q.round;
@@ -638,62 +659,126 @@ void AIMain()
 	// ==========[MAIN AI PART BEGIN]==========
 	// 
 	// 
-	int j=my_base->fuel; // j: usable fuel
-	for(int i=0;i<INFO->production_num;++i){
-		j-=(kProperty[INFO->production_list[i].unit_type].fuel_max
-			-((INFO->production_list[i].unit_type==FIGHTER ||
-			INFO->production_list[i].unit_type==SCOUT)?1:0)-((INFO->production_list[i].unit_type==CARGO)?150:0));
-	}
-	{
-		int c=0;
-		for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
-			if(i->second->team==my_team && i->second->type==CARGO)
-				++c;
-		}
+	if(Distance(my_base->pos,enemy_base->pos)<=93){
+		int j=my_base->fuel; // j: usable fuel
 		for(int i=0;i<INFO->production_num;++i){
-			if(INFO->production_list[i].unit_type==CARGO)
+			j-=(kProperty[INFO->production_list[i].unit_type].fuel_max
+				-((INFO->production_list[i].unit_type==FIGHTER ||
+				INFO->production_list[i].unit_type==SCOUT)?1:0)-((INFO->production_list[i].unit_type==CARGO)?150:0));
+		}
+		{
+			int c=0;
+			for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+				if(i->second->team==my_team && i->second->type==CARGO)
+					++c;
+			}
+			for(int i=0;i<INFO->production_num;++i){
+				if(INFO->production_list[i].unit_type==CARGO)
+					++c;
+			}
+			while(j>=150 && c<min(3+INFO->round/5,oceanNearMyBase.size())){
+				Produce(CARGO);
 				++c;
-		}
-		while(c<min(2+INFO->round/5,oceanNearMyBase.size())){
-			Produce(CARGO);
-			++c;
-			j-=150;
-		}
-	}
-	for(int i=0;i<min(my_base->metal/kProperty[FIGHTER].cost,
-		j/(kProperty[FIGHTER].fuel_max-1));++i){
-			Produce(FIGHTER);
-	}
-	{									// if fighter_with_no_job>13 then attack a given pos
-		unsigned fighter_with_no_job=0;
-		for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
-			Unit& obj=*(i->second);
-			if(obj.type==FIGHTER && obj.job.empty() && obj.last_seen==INFO->round){
-				++fighter_with_no_job;
+				j-=150;
 			}
 		}
-		int fighter_in_production_list=0;
-		for(int i=0;i<INFO->production_num;++i){
-			if(INFO->production_list[i].unit_type==FIGHTER && INFO->production_list[i].round_left<=1)
-				++fighter_in_production_list;
+		for(int i=0;i<min(my_base->metal/kProperty[FIGHTER].cost,
+			j/(kProperty[FIGHTER].fuel_max-1));++i){
+				Produce(FIGHTER);
 		}
-		if(fighter_with_no_job>=MAX_FIGHTER_WAITING  && !HasGSig(BASE_UNDER_ATTACT) && fighter_in_production_list>=4){
+		{									// if fighter_with_no_job>13 then attack a given pos
+			unsigned fighter_with_no_job=0;
 			for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
 				Unit& obj=*(i->second);
-				if(obj.type==FIGHTER && obj.job.empty()){
-					Job a;
-					a.type=a.ATTACK;
-					a.target=enemy_base->index;
-					a.pos=obj.pos+(enemy_base->pos-my_base->pos);
-					obj.job.push(a);
+				if(obj.type==FIGHTER && Distance(obj.pos,my_base->pos)<8){
+					++fighter_with_no_job;
+				}
+			}
+			int fighter_in_production_list=0;
+			for(int i=0;i<INFO->production_num;++i){
+				if(INFO->production_list[i].unit_type==FIGHTER && INFO->production_list[i].round_left<=0)
+					++fighter_in_production_list;
+			}
+			if(fighter_with_no_job>=MAX_FIGHTER_WAITING  && !HasGSig(BASE_UNDER_ATTACK) && fighter_in_production_list>=4){
+				for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+					Unit& obj=*(i->second);
+					if(obj.type==FIGHTER && obj.job.empty()){
+						Job a;
+						a.type=a.ATTACK;
+						a.target=enemy_base->index;
+						a.pos=obj.pos+(enemy_base->pos-my_base->pos);
+						obj.job.push(a);
+					}
 				}
 			}
 		}
+		for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+			Unit& obj=*(i->second);
+			if(obj.type==CARGO && obj.job.empty()){
+				obj.SimpleInit();
+			}
+		}
 	}
-	for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
-		Unit& obj=*(i->second);
-		if(obj.type==CARGO && obj.job.empty()){
-			obj.SimpleInit();
+	else{
+		int j=my_base->fuel; // j: usable fuel
+		for(int i=0;i<INFO->production_num;++i){
+			j-=(kProperty[INFO->production_list[i].unit_type].fuel_max
+				-((INFO->production_list[i].unit_type==FIGHTER ||
+				INFO->production_list[i].unit_type==SCOUT)?1:0)-((INFO->production_list[i].unit_type==CARGO)?150:0));
+		}
+		{
+			int c=0;
+			for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+				if(i->second->team==my_team && i->second->type==CARGO)
+					++c;
+			}
+			for(int i=0;i<INFO->production_num;++i){
+				if(INFO->production_list[i].unit_type==CARGO)
+					++c;
+			}
+			while(j>=150 && c<min(INFO->round<=1?6:12,oceanNearMyBase.size())){
+				Produce(CARGO);
+				++c;
+				j-=150;
+			}
+		}
+		int free_submarine=0;
+
+		for(int i=0;i<min(my_base->metal/kProperty[SUBMARINE].cost,
+			min(j/(kProperty[SUBMARINE].fuel_max),6));++i){
+				Produce(FIGHTER);
+		}
+		{									// if fighter_with_no_job>13 then attack a given pos
+			unsigned fighter_with_no_job=0;
+			for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+				Unit& obj=*(i->second);
+				if(obj.type==FIGHTER && obj.job.empty() && obj.last_seen==INFO->round){
+					++fighter_with_no_job;
+				}
+			}
+			int fighter_in_production_list=0;
+			for(int i=0;i<INFO->production_num;++i){
+				if(INFO->production_list[i].unit_type==FIGHTER && INFO->production_list[i].round_left<=1)
+					++fighter_in_production_list;
+			}
+			if(fighter_with_no_job>=MAX_FIGHTER_WAITING  && !HasGSig(BASE_UNDER_ATTACK) && fighter_in_production_list>=4){
+				for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+					Unit& obj=*(i->second);
+					if(obj.type==FIGHTER && obj.job.empty()){
+						Job a;
+						a.type=a.ATTACK;
+						a.target=enemy_base->index;
+						a.pos=obj.pos+(enemy_base->pos-my_base->pos);
+						obj.job.push(a);
+					}
+				}
+			}
+		}
+		for(map<int,Unit*>::iterator i=Units.begin();i!=Units.end();++i){
+			Unit& obj=*(i->second);
+			if(obj.type==CARGO && obj.job.empty()){
+				obj.SimpleInit();
+			}
 		}
 	}
 	//
@@ -707,3 +792,31 @@ void AIMain()
 			i->second->DoJob();
 	}
 }
+
+
+/*
+
+处理伤害<0情形
+预估敌方生命值
+基地维修、补弹药
+初始运输船向基地补给
+初始运输船数量
+资源空时不造运输船/运输船停至对方基地
+
+
+临海的己方据点视为矿
+
+*/
+
+/*
+
+先造运输船，6个，后期格子数
+航母，6个，优先，外移
+天气<=-3造侦察机（20回合）
+飞机，4架，第10回合开始造
+潜艇，12个，外移
+
+航母尽量不要被据点打
+
+
+*/
